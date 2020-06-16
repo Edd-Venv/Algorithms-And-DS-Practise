36649338c42bd50e5f03c15b4731e2e6564d7b2c
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-06-16 12:06:58 +0200

    ReverseString Complete

--- SectionOne/ReverseString.test.js
+++ SectionOne/ReverseString.test.js
@@ -1,9 +1,17 @@
-function revrse() {}
+function revrse(text) {
+  if (typeof text !== "string") return "Not A String";
+  let reversedText = "";
+  for (let i = text.length - 1; i >= 0; i--) {
+    reversedText += text[i];
+  }
+  return reversedText;
+}
 
 describe("SECTION ONE, REVERSE-STRING", () => {
   it("SHOULD RETURN A NEW STRING WITH THE REVERSED ORDER OF CHARACTERS", () => {
-    expect(revrse("apple").toMatch("leppa"));
-    expect(revrse("hello").toMatch("olleh"));
-    expect(revrse("Greetings!").toMatch("!sgniteerG"));
+    expect(revrse(434)).toMatch("Not A String");
+    expect(revrse("apple")).toMatch("elppa");
+    expect(revrse("hello")).toMatch("olleh");
+    expect(revrse("Greetings!")).toMatch("!sgniteerG");
   });
 });
