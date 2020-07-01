b0275b8b487651e9ec77397bb6ca5a154a718c08
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-07-01 12:00:32 +0200

    ReverseInt Complete

--- SectionOne/reverseInt.test.js
+++ SectionOne/reverseInt.test.js
@@ -1,9 +1,43 @@
+const reverseNegativeNumber = (number) => {
+  let stringVersionOfNumber = `${number}`;
+  let stringVersionOfReversedNumber = "-";
+
+  for (let i = stringVersionOfNumber.length - 1; i > 0; i--) {
+    stringVersionOfReversedNumber += stringVersionOfNumber[i];
+  }
+
+  return +stringVersionOfReversedNumber;
+};
+
+const reversePositiveNumber = (number) => {
+  let stringVersionOfNumber = `${number}`;
+  let stringVersionOfReversedNumber = "";
+  for (let i = stringVersionOfNumber.length - 1; i >= 0; i--) {
+    stringVersionOfReversedNumber += stringVersionOfNumber[i];
+  }
+
+  return +stringVersionOfReversedNumber;
+};
+
+function reverseInt(number) {
+  if (typeof number === "number") {
+    const stringVersionOfNumber = `${number}`;
+
+    if (stringVersionOfNumber.includes("-")) {
+      return reverseNegativeNumber(number);
+    } else return reversePositiveNumber(number);
+  }
+
+  return "NaN";
+}
+
 describe("SECTION ONE, REVERSE-INTERGER", () => {
   it("SHOULD REVERSE THE ORDERING OF NUMBERS", () => {
     expect(reverseInt(15)).toBe(51);
     expect(reverseInt(981)).toBe(189);
-    expect(reverseInt(500)).toBe(500);
+    expect(reverseInt(500)).toBe(5);
     expect(reverseInt(-15)).toBe(-51);
     expect(reverseInt(-90)).toBe(-9);
+    expect(reverseInt("gsgf")).toMatch("NaN");
   });
 });
