01b65f2c3a25ba847e58b9df3857a82fdab37a9c
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-07-03 14:16:22 +0200

    Max Character Complete

--- MaxChar.test.js
+++ MaxChar.test.js
@@ -1,3 +1,21 @@
+function maxChar(string) {
+  const hashMap = {};
+  let maxChar = "";
+  let maxCount = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
+
+    hashMap[char] = hashMap[char] + 1 || 1;
+    if (hashMap[char] > maxCount) {
+      maxChar = char;
+      maxCount = hashMap[char];
+    }
+  }
+
+  return maxChar;
+}
+
 describe("SECTION ONE, MAX-CHAR", () => {
   it("SHOULD RETURN THE CHARACTER THAT IS MOST COMMONLY USED IN THE STRING", () => {
     expect(maxChar("abccccccd")).toMatch("c");
