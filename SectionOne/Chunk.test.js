8b1919511d9b20c48f182412e95f0feac6dffa24
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-07-07 13:05:49 +0200

    Chunk Complete

--- SectionOne/Chunk.test.js
+++ SectionOne/Chunk.test.js
@@ -1,4 +1,27 @@
-describe("SECTION ONE, CHUNCK", () => {
+function chunk(arr, size) {
+  const chunk = [];
+  let subArray = [];
+  let count = 0;
+
+  while (count < arr.length) {
+    let j = 0;
+
+    for (j; j < size; j++) {
+      if (arr[count] != undefined) {
+        subArray.push(arr[count]);
+        count++;
+      }
+    }
+
+    chunk.push(subArray);
+    subArray = [];
+    j = 0;
+  }
+
+  return chunk;
+}
+
+describe("SECTION ONE, CHUNK", () => {
   it("SHOULD RETURN AN ARRAY OF ARRAYS", () => {
     expect(chunk([1, 2, 3, 4, 5], 2)).toEqual(
       expect.arrayContaining([[1, 2], [3, 4], [5]])
--- SectionOne/MaxChar.test.js
+++ SectionOne/MaxChar.test.js
@@ -16,7 +16,7 @@ function maxChar(string) {
   return maxChar;
 }
 
-describe("SECTION ONE, MAX-CHAR", () => {
+describe.skip("SECTION ONE, MAX-CHAR", () => {
   it("SHOULD RETURN THE CHARACTER THAT IS MOST COMMONLY USED IN THE STRING", () => {
     expect(maxChar("abccccccd")).toMatch("c");
     expect(maxChar("apple 1231111")).toMatch("1");
