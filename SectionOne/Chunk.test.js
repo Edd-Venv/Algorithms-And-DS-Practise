41fee07a049de064feb6917311e11d51451ba057
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-07-07 12:13:49 +0200

    Chunk test set Up

--- SectionOne/Chunk.test.js
+++ SectionOne/Chunk.test.js
@@ -1,5 +1,17 @@
 describe("SECTION ONE, CHUNCK", () => {
   it("SHOULD RETURN AN ARRAY OF ARRAYS", () => {
-    expect(chunk([1, 2, 3, 4, 5])).toEqual(expect.any([[1, 2], [3, 4], [5]]));
+    expect(chunk([1, 2, 3, 4, 5], 2)).toEqual(
+      expect.arrayContaining([[1, 2], [3, 4], [5]])
+    );
+    expect(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3)).toEqual(
+      expect.arrayContaining([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8],
+      ])
+    );
+    expect(chunk([1, 2, 3, 4, 5], 4)).toEqual(
+      expect.arrayContaining([[1, 2, 3, 4], [5]])
+    );
   });
 });
