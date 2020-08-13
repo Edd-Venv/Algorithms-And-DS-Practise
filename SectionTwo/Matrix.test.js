240c0cae0c73d453e30d039a1bbcb1613fd39e86
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-08-13 16:54:02 +0200

    Matrix Complete

--- SectionTwo/Matrix.test.js
+++ SectionTwo/Matrix.test.js
@@ -1,3 +1,48 @@
+function matrixFun(number) {
+  const results = [];
+
+  for (let i = 0; i < number; i++) {
+    results.push([]);
+  }
+
+  let counter = 1;
+  let startColumn = 0;
+  let endColumn = number - 1;
+  let startRow = 0;
+  let endRow = number - 1;
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // Top Row
+    for (let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+
+    // Right Column
+    for (let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter;
+      counter++;
+    }
+    endColumn--;
+
+    //Bottom Row
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+
+    //Start Column
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter;
+      counter++;
+    }
+    startColumn++;
+  }
+  return results;
+}
+
 describe("SECTION TWO, ", () => {
   it("SHOULD RETURN AN NXN SPIRAL MATRIX", () => {
     expect(matrixFun(2)).toEqual([
@@ -13,6 +58,7 @@ describe("SECTION TWO, ", () => {
       [1, 2, 3, 4],
       [12, 13, 14, 5],
       [11, 16, 15, 6],
+      [10, 9, 8, 7],
     ]);
   });
 });
--- SectionTwo/Vowels.test.js
+++ SectionTwo/Vowels.test.js
@@ -20,7 +20,7 @@ function vowels(str) {
   return matches ? matches.length : 0;
 }
 
-describe("SECTION TWO", () => {
+describe.skip("SECTION TWO", () => {
   it("SHOULD RETURN THE TOTAL NUMBER OF TIMES VOWELS APPEAR IN A SENTENCE", () => {
     expect(vowels("Hi There!")).toBe(3);
     expect(vowels("Why do you ask?")).toBe(4);
