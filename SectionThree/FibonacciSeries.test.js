3189ca18421bd7fec77021179ec11c19e56a71d1
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-08-26 12:43:33 +0200

    Recursive Fib Complete

--- SectionThree/FibonacciSeries.test.js
+++ SectionThree/FibonacciSeries.test.js
@@ -1,10 +1,25 @@
 /*
 // Recursive Solution
-function fibonacci(number) {
+function slowFibonacci(number) {
   if (number < 2) return number;
   else return fibonacci(number - 1) + fibonacci(number - 2);
   //Time Complexity = O(n^2)
+  //Time Complexity = o(n) with memoization
 }
+
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  };
+}
+const fibonacci = memoize(slowFibonacci);
+
 */
 
 function fibonacci(number) {
