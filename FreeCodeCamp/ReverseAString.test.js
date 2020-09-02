1f282e2ae338fadf501530a1f0f7fb8c274c7b3e
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-09-02 10:18:01 +0200

    Reverse String Complete

--- FreeCodeCamp/ReverseAString.test.js
+++ FreeCodeCamp/ReverseAString.test.js
@@ -1,8 +1,19 @@
-describe("FREECODECAMP", () => {
+function reverseString(str) {
+  if (typeof str !== "string") return;
+  let reversedString = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i];
+  }
+  return reversedString;
+  //Time Complexity = O(n);
+}
+
+describe.skip("FREECODECAMP", () => {
   it("SHOULD REVERSE A STRING", () => {
     expect(reverseString("hello")).toMatch("olleh");
-    expect(reverseString("howdy")).toMatch("ydwoH");
-    exepct(reverseString("Greetings from Earth")).toMatch(
+    expect(reverseString("Howdy")).toMatch("ydwoH");
+    expect(reverseString("Greetings from Earth")).toMatch(
       "htraE morf sgniteerG"
     );
   });
