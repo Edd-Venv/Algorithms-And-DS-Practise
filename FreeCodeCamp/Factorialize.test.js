a763f0d6b62cdaee3d702b3e2e1ba5f4c45f59df
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-09-08 09:26:34 +0200

    to be continued

--- FreeCodeCamp/Factorialize.test.js
+++ FreeCodeCamp/Factorialize.test.js
@@ -11,22 +11,21 @@
     //Time Complexity = O(n);
 }*/
 
-function factorialize(number, prevNumber = 1) { 
-     if (typeof number !== "number") return;
-     
-     if (number === 0) return prevNumber;
-     
-     let factorial = number * prevNumber;
-         
-     return factorialize(number - 1, factorial); 
+function factorialize(number, prevNumber = 1) {
+  if (typeof number !== "number") return;
+
+  if (number === 0) return prevNumber;
+
+  let factorial = number * prevNumber;
+
+  return factorialize(number - 1, factorial);
 }
 
-
-describe("FREECODECAMP",  () => {
-    it("SHOULD RETURN THE FACTORIAL OF A GIVEN NUMBER", () => {
-        expect(factorialize(5)).toBe(120);
-        expect(factorialize(10)).toBe(3628800);
-        expect(factorialize(20)).toBe(2432902008176640000);
-        expect(factorialize(0)).toBe(1);
-    })
-})
+describe.skip("FREECODECAMP", () => {
+  it("SHOULD RETURN THE FACTORIAL OF A GIVEN NUMBER", () => {
+    expect(factorialize(5)).toBe(120);
+    expect(factorialize(10)).toBe(3628800);
+    expect(factorialize(20)).toBe(2432902008176640000);
+    expect(factorialize(0)).toBe(1);
+  });
+});
--- FreeCodeCamp/FindTheLongestWordLength.test.js
+++ FreeCodeCamp/FindTheLongestWordLength.test.js
@@ -1,9 +1,40 @@
+function findLongestWordLength(str) {
+  if (typeof str !== "string") return;
+  let length = 0;
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i + 1] === " ") {
+      //console.log(count, length, str[i]);
+      if (count > length) {
+        length = count;
+      }
+
+      count = 0;
+      console.log(count, length, str[i]);
+    }
+    //console.log(count, length, str[i]);
+    count++;
+  }
+  return length;
+}
+
 describe("FREECODECAMP", () => {
-    it("SHOULD RETURN THE LENGTH OF THE LONGEST WORD IN A STRING", ()=>{
-        expect(findLongestWordLengthWordLength("The quick brown fox jumped over the lazy dog")).toBe(6);
-        expect(findLongestWordLengthWordLength("May the force be with you")).toBe(5);
-        expect(findLongestWordLengthWordLength("Google do a barrel roll")).toBe(6);
-        expect(findLongestWordLengthWordLength("What is the average airspeed velocity of an unladen swallow")).toBe(8);
-        expect(findLongestWordLengthWordLength("What if we try a super-long word such as otorhinolaryngology")).toBe(19);
-    })
-})
+  it("SHOULD RETURN THE LENGTH OF THE LONGEST WORD IN A STRING", () => {
+    expect(
+      findLongestWordLength("The quick brown fox jumped over the lazy dog")
+    ).toBe(6);
+    expect(findLongestWordLength("May the force be with you")).toBe(5);
+    expect(findLongestWordLength("Google do a barrel roll")).toBe(6);
+    expect(
+      findLongestWordLength(
+        "What is the average airspeed velocity of an unladen swallow"
+      )
+    ).toBe(8);
+    expect(
+      findLongestWordLength(
+        "What if we try a super-long word such as otorhinolaryngology"
+      )
+    ).toBe(19);
+  });
+});
